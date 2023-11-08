c2734fdf4169c3f56248b04f985671ce785f4998
fmilke <fridjofmilke@gmail.com>
2023-11-08 16:59:24 +0100

    Promisified test script

--- tree-sitter-c4script/test-original.js
+++ tree-sitter-c4script/test-original.js
@@ -1,25 +1,50 @@
 const fs = require('fs');
 const path = require('path');
-const {execSync, spawnSync} = require('child_process');
+const { spawn } = require('child_process');
 
 const fixturesDir = './test/original_scripts/';
 
-const fnames = fs.readdirSync(fixturesDir);
+const fnames = fs.readdirSync(fixturesDir).slice();
 
 let successful = 0;
 
-for (const fname of fnames) {
-    const fixturePath = path.join(fixturesDir, fname);
+function spawnAsync(cmd, args) {
+    return new Promise((res, rej) => {
+        const cp = spawn(cmd, args, {
+            stdio: 'ignore',
+        });
 
-    console.log('Checking: ' + fixturePath);
-
-    const r = spawnSync('npx', ['tree-sitter', 'parse', fixturePath]);
-
-    console.log(`Status: ${r.status}; Error: ${r.error?.toString() ?? 'none'}`);
-    
-    if (r.status === 0) {
-        successful++;
-    }
+        cp.once('error', rej);
+        cp.once('exit', (code, _sig) => res(code));
+    });
 }
 
-console.log(`From ${fnames.length} total, ${successful} were successfully parsed`);
+Promise.allSettled(fnames.map(async fname => {
+    const fixturePath = path.join(fixturesDir, fname);
+
+    let log = 'Checking: ' + fixturePath;
+
+    let code;
+    try {
+        code = await spawnAsync('npx', ['tree-sitter', 'parse', fixturePath]);
+    } catch (e) {
+        console.error(`Error parsing '${fixturePath}': ${e}`);
+        return -1;
+    }
+
+    log += `Status: ${code}`;
+    
+    if (code === 0) {
+        successful++;
+    }
+
+    return log;
+})).then(results => {
+
+    results.forEach(p => {
+        console.log(p.value);
+    });
+    
+    console.log(`From ${fnames.length} total, ${successful} were successfully parsed`);
+
+});
